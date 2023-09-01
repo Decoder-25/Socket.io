141b830fe8e4256bdd733cd87408785b2f2bb7b7
Disha <bidisha2125@gmail.com>
2023-09-01 19:51:45 +0530

    Basic socket connection has done

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const http = require('http');
+const socketio = require("socket.io");
+
 const app = express()
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use('/', express.static(path.join(__dirname, + '/public')));
+io.on('connection', (socket) => {
+    console.log('A user connected', socket.id);
+})
 
-app.listen(3000, () => {
+app.use('/', express.static(__dirname + '/public'));
+
+server.listen(3050, () => {
     console.log('server started')
 })
