1dd020fb9cdb66b17ca29a88e8a7028d0994800c
Disha <bidisha2125@gmail.com>
2023-09-01 20:45:02 +0530

    Done with the setup

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const io = socketio(server);
 
 io.on('connection', (socket) => {
     console.log('A user connected', socket.id);
+
+    socket.on('from_client', () => {
+        console.log("event coming from client");
+    })
+
+    setInterval(() => {
+        socket.emit('from_server');
+    }, 2000);
 })
 
 app.use('/', express.static(__dirname + '/public'));
